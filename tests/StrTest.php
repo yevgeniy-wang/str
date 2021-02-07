b7b3a6803a26bce752b8b67d3e4e024b2aad0e5f
Stanislav Protasevich <stanislav.protasevich@gmail.com>
2021-02-07 09:52:38 +0200

    @todo after method

--- tests/StrTest.php
+++ tests/StrTest.php
@@ -2,9 +2,14 @@
 
 namespace Hillel\Str\Tests;
 
+use Hillel\Str\Str;
 use PHPUnit\Framework\TestCase;
 
 class StrTest extends TestCase
 {
-
+    public function testAfter()
+    {
+        $this->assertEquals('This is my name', Str::after('This is my name', 'something'));
+        $this->assertEquals(' my name', Str::after('This is my name', 'This is'));
+    }
 }
